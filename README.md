8cde92fcc1ea6108c73c6caf0ebc558dd04c973a
Jonerie 123 hehehe <154795227+Joneriehahahaha@users.noreply.github.com>
2024-05-07 19:22:19 +0800

    Add files via upload
    Spokiffy

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Spo-kiffy
+![image](https://github.com/Joneriehahahaha/IPT-Spotify/blob/main/438069815_762090616013474_4139456549786851921_n.jpg)
+
+WALA PAY CHAT 
+
+
+
+--FRONT END SET UP
+
+npm install
+
+npm run dev
+
+--SERVER SET UP
+
+
+npm install
+
+
+node index.js
+
+
+
+
---
+++ frontend/src/components/ChatComponent.vue
@@ -0,0 +1,8 @@
+<template>
+   <div class="text-center text-lg font-bold mb-4 w-full mx-2">Chat Box</div>
+
+   <div class="fixed bottom-0 justify-center p-4 ml-6">
+   <input type="text" placeholder="Enter a message" class="rounded-full text-center border border-white mb-4 h-8 w-full">
+   <button class="bg-green-600 text-white text-md font-semibold rounded-full h-8 w-full text-center">Send Message</button>
+</div>
+</template>
